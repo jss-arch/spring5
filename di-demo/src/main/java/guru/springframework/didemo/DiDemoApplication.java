eddead32f917e293ebd998e203890fb7ede8b300
jss-arch <jorge_souzadasilva@outlook.com>
2019-12-28 16:48:35 -0300

    Adicionado projeto de exemplo de application context

---
+++ di-demo/src/main/java/guru/springframework/didemo/DiDemoApplication.java
@@ -0,0 +1,21 @@
+package guru.springframework.didemo;
+
+import guru.springframework.didemo.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = SpringApplication.run(DiDemoApplication.class, args);
+
+        MyController controller = (MyController) context.getBean("myController");
+
+        controller.hello();
+
+    }
+
+}
