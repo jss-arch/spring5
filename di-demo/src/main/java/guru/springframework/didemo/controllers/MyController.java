ab1b94ce4e74deaeeab28b345e82864cc3d194f9
jss-arch <jorge_souzadasilva@outlook.com>
2019-12-29 15:14:51 -0300

    Demo de injecao de dependencias completo

--- di-demo/src/main/java/guru/springframework/didemo/controllers/MyController.java
+++ di-demo/src/main/java/guru/springframework/didemo/controllers/MyController.java
@@ -1,13 +1,19 @@
 package guru.springframework.didemo.controllers;
 
+import guru.springframework.didemo.service.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
-    public String hello() {
-        System.out.println("Hello");
+    private GreetingService greetingService;
 
-        return "teste";
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String hello() {
+
+        return greetingService.sayGreeting();
     }
 }
