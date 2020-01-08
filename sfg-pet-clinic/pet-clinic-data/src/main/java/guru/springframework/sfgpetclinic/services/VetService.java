51756d2051844100485d45e7fe7db413e119c1fb
jss-arch <jorge_souzadasilva@outlook.com>
2020-01-08 14:48:36 -0300

    Adicionado bootstrap ao petclinic. Closes #29

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -6,6 +6,4 @@ import java.util.Set;
 
 public interface VetService extends CrudService<Vet, Long> {
 
-    Vet findByLastName(String lastName);
-
 }
---
+++ spring5-di-demo/src/main/java/guru/springframework/config/GreetingServiceConfig.java
@@ -0,0 +1,42 @@
+package guru.springframework.config;
+
+import guru.springframework.services.GreetingRepository;
+import guru.springframework.services.GreetingService;
+import guru.springframework.services.GreetingServiceFactory;
+import guru.springframework.services.PrimaryGreetingService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    GreetingServiceFactory greetingServiceFactory(GreetingRepository greetingRepository) {
+        return new GreetingServiceFactory(greetingRepository);
+    }
+
+    @Bean
+    @Primary
+    @Profile({"en", "default"})
+    GreetingService primaryGreetingService(GreetingServiceFactory greetingServiceFactory) {
+        return greetingServiceFactory.createGreetingService("en");
+    }
+
+    @Bean
+    @Profile("es")
+    @Primary
+    GreetingService primarySpanishGreetingService(GreetingServiceFactory greetingServiceFactory) {
+        return greetingServiceFactory.createGreetingService("es");
+    }
+
+    @Bean
+    @Profile("de")
+    @Primary
+    GreetingService primaryGermanGreetingService(GreetingServiceFactory greetingServiceFactory) {
+        return greetingServiceFactory.createGreetingService("de");
+    }
+
+}
